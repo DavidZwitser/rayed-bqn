03a02fee1e96cc9d6e06157d9d1d5d95c5e6976c
davidzwitser <davidzwitser@gmail.com>
2024-07-10 16:39:26 +0200

    Added flake

---
+++ flake.nix
@@ -0,0 +1,37 @@
+{
+  description = "Rayed BQN";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+    let
+
+      pkgs = nixpkgs.legacyPackages.${system};
+      raylib = pkgs.raylib;
+
+    in rec {
+      packages.default = pkgs.stdenv.mkDerivation {
+        name = "RayedBQN";
+        src = ./.;
+
+        nativeBuildInputs = [ pkgs.git ];
+
+        buildPhase = /*bash*/ ''
+          # Overwriting the default raylib loading
+          echo "raylibheaderpath ⇐ •file.At \"${raylib}/include/raylib.h\"" > ./config.bqn
+          echo "rayliblibpath ⇐ •file.At \"${raylib}/lib/libraylib.dylib\"" >> ./config.bqn
+          echo "⟨raylibheaderpath ⋄ rayliblibpath⟩ ⇐ •Import \"../config.bqn\"" > ./src/loadConfig.bqn
+        '';
+
+        installPhase = /*bash*/ ''
+          mkdir -p $out
+          cp -r $src/src $out
+          cp -r $src/ffi $out
+        '';
+      };
+  });
+}
